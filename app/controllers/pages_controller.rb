8bc7b2ab42d2a5bf3f326042eb24b4ba53cb355d
sandeep-freshdesk <sandeep@freshdesk.com>
2017-01-20 18:23:50 +0530

    added paths all anchor tages via  link_to method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,11 +8,22 @@ class PagesController < ApplicationController
   def contact
   	puts "came in contact method"
   	@title = "Contact"
+    puts "**************************"
+    puts "**************************"
+    puts "**************************"
+    puts @contact
+    puts @contact_url
+    puts @contact_path
+    puts "**************************"
+    puts "**************************"
+    puts "**************************"
+    puts "**************************"
   end
 
   def about
-	 puts "came in about method"
-  	@title = "About"
+    puts "came in about method"
+    @title = "About"
+
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
   SampleApp::Application.routes.draw do
 
   match '/contact', :to => 'pages#contact'
+
+  match '/about', :to => 'pages#about'
+
+  match '/help', :to => 'pages#help'
   
+  match '/home', :to => 'pages#home'
+
   root :to => 'pages#home'
 
 
