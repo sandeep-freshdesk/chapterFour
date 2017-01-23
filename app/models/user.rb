b55a11cd505a55cce3b6d4781ff4812233f84867
sandeep-freshdesk <sandeep@freshdesk.com>
2017-01-23 18:29:05 +0530

    adding files for creating user model and show user details on browser till chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,6 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email
 
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :maximum => 50 }
+	validates :email, :presence => true, :uniqueness =>  { :case_sensitive => false }
 end
