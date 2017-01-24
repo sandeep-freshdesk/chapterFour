35a8bef15b7b433245b169cdabd5e23a3e2124dd
sandeep-freshdesk <sandeep@freshdesk.com>
2017-01-24 20:01:04 +0530

    adding files for sign up, end of chapter eight

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,42 @@ class UsersController < ApplicationController
 	@title = @user.name
   end
 
+  def create
+	@user = User.new(params[:user])
+		if @user.save
+		# Handle a successful save.
+
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("sign up sssuuccceesssssfffuuuulllll");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("sign up fail");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			puts("*********************");
+			@title = "Sign up"
+			render 'new'
+		end
+	end
+
+
 end
