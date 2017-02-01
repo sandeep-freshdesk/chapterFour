6f01f59be18a86e2965badbf30cf90462ff5f788
sandeep-freshdesk <sandeep@freshdesk.com>
2017-02-01 15:43:13 +0530

    changed delete link_to to button_to for destroy user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user, :only => :destroy
+  # before_filter :admin_user, :only => :destroy
 
   def new
 	@user = User.new
